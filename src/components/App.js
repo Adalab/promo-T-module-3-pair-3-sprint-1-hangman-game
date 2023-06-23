7c6ff7c21c42441456249cd61c39a4127d61f638
raquelgarciat <raquelgarciat@gmail.com>
2023-06-23 11:31:57 +0200

    modify state array

--- src/components/App.js
+++ src/components/App.js
@@ -6,36 +6,48 @@ function App() {
   const [numberOfErrors, setNumberOfErrors] = useState('0');
   const [letter, setLetter] = useState('');
   const [word, setword] = useState('katakroker');
-  
+  const [userLetters, setUserLetters] = useState([]);
+
+  //if (word.includes(userLetters)) 
 
   const renderSolutionLetters = () => {
-    
     const wordLetters = word.split('');
-    return wordLetters.map((oneletter, index) => {
-      return(
-      <li key={index} className='letter'>{oneletter}</li>
-      )
-      
-    });
-  }
 
+    return wordLetters.map((oneletter, index) => {
+      oneletter = '';
+      console.log(oneletter);
+      if (word.includes(userLetters)) {
+      return (
+        <li key={index} className="letter">
+          {letter}
+        </li>
+      );
+    } else {
+      return (
+        <li key={index} className="letter">
+          ''
+        </li>
+      );
+    }});
+  };
 
   const handleClick = (event) => {
     event.preventDefault();
     setNumberOfErrors(parseInt(numberOfErrors) + parseInt(1));
-
-   
-
   };
 
   const handleInputChange = (event) => {
     //event.preventDefault();
     const result = event.target.value.replace(/[^a-z]/gi, '');
     setLetter(result);
+    const newUserLetters = [...userLetters];
+    newUserLetters.push(letter);
+    //if (result === '') {
+    //userLetters.push(letter);}
     
   };
 
-    /*const handleInput = (ev) => {
+  /*const handleInput = (ev) => {
     ev.preventDefault();
     const inputValue = ev.target.value;
 
@@ -50,17 +62,17 @@ function App() {
   };*/
 
   return (
-    <div className='page'>
+    <div className="page">
       <header>
-        <h1 className='header__title'>Juego del ahorcado</h1>
+        <h1 className="header__title">Juego del ahorcado</h1>
       </header>
-      <main className='main'>
+      <main className="main">
         <section>
-          <div className='solution'>
-            <h2 className='title'>Solución:</h2>
-           
-            <ul className='letters'>
-            {renderSolutionLetters()}
+          <div className="solution">
+            <h2 className="title">Solución:</h2>
+
+            <ul className="letters">
+              {renderSolutionLetters()}
               {/*<li className='letter'>k</li>
               <li className='letter'>a</li>
               <li className='letter'>{letter}</li>
@@ -73,29 +85,29 @@ function App() {
   <li className='letter'>r</li>*/}
             </ul>
           </div>
-          <div className='error'>
-            <h2 className='title'>Letras falladas:</h2>
-            <ul className='letters'>
-              <li className='letter'>f</li>
-              <li className='letter'>q</li>
-              <li className='letter'>h</li>
-              <li className='letter'>p</li>
-              <li className='letter'>x</li>
+          <div className="error">
+            <h2 className="title">Letras falladas:</h2>
+            <ul className="letters">
+              <li className="letter">f</li>
+              <li className="letter">q</li>
+              <li className="letter">h</li>
+              <li className="letter">p</li>
+              <li className="letter">x</li>
             </ul>
           </div>
-          <form className='form'>
-            <label className='title' htmlFor='last-letter'>
+          <form className="form">
+            <label className="title" htmlFor="last-letter">
               Escribe una letra:
             </label>
             <input
-              autocomplete='off'
-              className='form__input'
-              maxlength='1'
-              type='text'
-              name='last-letter'
-              id='last-letter'
+              autocomplete="off"
+              className="form__input"
+              maxlength="1"
+              type="text"
+              name="last-letter"
+              id="last-letter"
               value={letter}
-              pattern='[A-Za-z]'
+              pattern="[A-Za-z]"
               //onInput={handleInput}
               onChange={handleInputChange}
             />
@@ -103,19 +115,19 @@ function App() {
           <button onClick={handleClick}>Incrementar</button>
         </section>
         <section className={`dummy error-${numberOfErrors}`}>
-          <span className='error-13 eye'></span>
-          <span className='error-12 eye'></span>
-          <span className='error-11 line'></span>
-          <span className='error-10 line'></span>
-          <span className='error-9 line'></span>
-          <span className='error-8 line'></span>
-          <span className='error-7 line'></span>
-          <span className='error-6 head'></span>
-          <span className='error-5 line'></span>
-          <span className='error-4 line'></span>
-          <span className='error-3 line'></span>
-          <span className='error-2 line'></span>
-          <span className='error-1 line'></span>
+          <span className="error-13 eye"></span>
+          <span className="error-12 eye"></span>
+          <span className="error-11 line"></span>
+          <span className="error-10 line"></span>
+          <span className="error-9 line"></span>
+          <span className="error-8 line"></span>
+          <span className="error-7 line"></span>
+          <span className="error-6 head"></span>
+          <span className="error-5 line"></span>
+          <span className="error-4 line"></span>
+          <span className="error-3 line"></span>
+          <span className="error-2 line"></span>
+          <span className="error-1 line"></span>
         </section>
       </main>
     </div>
